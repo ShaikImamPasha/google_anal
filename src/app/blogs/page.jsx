489e97ce0e88c40cf34ab2a75c5751a717ec80c0
ShaikImamPasha <shaikimam916@gmail.com>
2025-01-19 11:37:42 +0530

    checkin main.

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,57 +1,57 @@
 import Script from "next/script";
 //import mockBlogData from "../../components/blogs/mockBlogData";
 
-const structuredData={
-    "@context": "https://schema.org",
-    "@type": "ItemList",
-    "itemListElement": [
+const structuredData = {
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name": "imam main page",
+  "alternateName": "imam main page",
+  "url": "https://www.nextbill.io/",
+  "logo": "https://www.nextbill.io/nextbill.png",
+  "description": "Take the hassle out of billing with NextBill, your go-to invoicing platform. With features like automated reminders and customizable templates, we simplify your finance management so you can focus on what you're great at.",
+  "contactPoint": {
+    "@type": "ContactPoint",
+    "telephone": "9958553533",
+    "contactType": "customer service",
+    "areaServed": "IN",
+    "availableLanguage": ["en","Hindi"]
+  },
+  "sameAs": [
+    "https://www.linkedin.com/company/nextbill/",
+    "https://www.instagram.com/nextbill_/",
+    "https://x.com/NextBill_"
+  ]
+}
+const currentUrl = `https://www.nextbill.io`;
+
+export const metadata = {
+  title: "meta title",
+  description: "meta des",
+  keywords: "nextbill,recurring invoice,invoice generation,invoicing process,creating invoice,generate e invoice,create the invoice,generate bill,invoice and payment,create billing invoice,invoice management system,invoice maker,online invoice generator,create invoice online,online bill generator",
+  openGraph: {
+    title: "meta title",
+    description: "met dessly in one place.",
+    images: [
       {
-        "@type": "ListItem",
-        "position": 1,
-        "item": {
-          "@type": "Course",
-          "url":"https://www.example.com/courses#intro-to-cs",
-          "name": "Introduction to Computer Science and Programming",
-          "description": "This is an introductory CS course laying out the basics.",
-          "provider": {
-            "@type": "Organization",
-            "name": "University of Technology - Example",
-            "sameAs": "https://www.example.com"
-         }
-        }
+        url: "https://www.nextbill.io/nextbill.png",
+        width: 800,
+        height: 600,
       },
-      {
-        "@type": "ListItem",
-        "position": 2,
-        "item": {
-          "@type": "Course",
-          "url":"https://www.example.com/courses#intermediate-cs",
-          "name": "Intermediate Computer Science and Programming",
-          "description": "This is a CS course that builds on the basics learned in the Introduction course.",
-          "provider": {
-            "@type": "Organization",
-            "name": "University of Technology - Example",
-            "sameAs": "https://www.example.com"
-         }
-       }
-      },
-      {
-        "@type": "ListItem",
-        "position": 3,
-        "item": {
-          "@type": "Course",
-          "url":"https://www.example.com/courses#advanced-cs",
-          "name": "Advanced Computer Science and Programming",
-          "description": "This CS course covers advanced programming principles.",
-          "provider": {
-            "@type": "Organization",
-            "name": "University of Technology - Eureka",
-            "sameAs": "https://www.example.com"
-         }
-        }
-      }
-    ]
+    ],
+  },
+  twitter: {
+    card: "eta title",
+    title: "eta des",
+    description:
+      "met des",
+    images: "https://www.nextbill.io/nextbill.png",
+  },
+  alternates: {
+    canonical: `${currentUrl}`,
   }
+  
+};
+
 export default function BlogsMainPage() {
   return (
     <div className="w-full  h-auto bg-[#f9fbfc] ">
