de7b067619c6efc13c0526d8447aa1ced8378d91
ShaikImamPasha <shaikimam916@gmail.com>
2025-04-09 10:34:25 +0530

    added logo ...new

--- src/app/page.js
+++ src/app/page.js
@@ -34,8 +34,8 @@ export const metadata = {
     images: [
       {
         url: "https://www.imam.live/Nextbill_final_logo_optimized.png",
-        width: 800,
-        height: 800,
+        width: 300,
+        height: 300,
       },
     ],
   },
@@ -47,8 +47,8 @@ export const metadata = {
     images: [
       {
         url: "https://www.imam.live/Nextbill_final_logo_optimized.png",
-        width: 800,
-        height: 800,
+        width: 300,
+        height: 300,
       },
     ],
   },
